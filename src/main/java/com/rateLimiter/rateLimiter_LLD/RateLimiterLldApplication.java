14509538caf085f4998c16b54ece08a4013f5728
Vaishnav Puram <puramv307@gmail.com>
2024-07-06 17:37:18 +0530

    initialized project

---
+++ src/main/java/com/rateLimiter/rateLimiter_LLD/RateLimiterLldApplication.java
@@ -0,0 +1,13 @@
+package com.rateLimiter.rateLimiter_LLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterLldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimiterLldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rateLimiter/rateLimiter_LLD/RateLimiterLldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rateLimiter.rateLimiter_LLD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterLldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
